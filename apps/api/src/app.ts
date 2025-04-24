bf192131f17fe562dad2f2e98d67c4b763f4163e
Sanjeev Shakya <sanjeevshakya@Sanjeevs-MacBook-Pro.local>
2025-04-24 18:56:54 +0545

    add prefix

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,13 +1,13 @@
 import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import router from "@api/routes.ts";
 import { errorHandler } from "./middleware/errorHandler.ts";
+import apiRouter from '@api/routes.ts';
 
 const app = new Application();
 
 // Middleware
 app.use(errorHandler);
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(apiRouter.routes());
+app.use(apiRouter.allowedMethods());
 
 // Start server
 const PORT = Deno.env.get("PORT") || "8000";
